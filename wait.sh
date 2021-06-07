f27262849cfd4caccfdd1cf43022c562df5043c2
Matt Halliday <bat@oksu.re>
2021-06-07 16:10:38 +0100

    Add timeout to mysql wait script

--- wait.sh
+++ wait.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+
+CHECK_DB='mysql -h 127.0.0.1 -P 3306 -uroot -p"magento2" -e "SHOW DATABASES;"'
+COUNTER=0
 echo "Waiting for MySQL"
-until mysql -h 127.0.0.1 -P 3306 -uroot -p"magento2" -e "SHOW DATABASES;" &> /dev/null
+until eval $CHECK_DB &> /dev/null
 do
   printf "."
   sleep 1
+  ((COUNTER++))
+  if [[ $COUNTER -eq 30 ]]; then
+    echo "\nMySQL did not start:"
+    eval $CHECK_DB
+    exit 1
+  fi
 done
+
 echo "\nMySQL ready"
